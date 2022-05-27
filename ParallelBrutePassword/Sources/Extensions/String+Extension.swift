976691ebf40a7ef22cf26d00567abfcd2a44f00f
Ilya Volkov <illvolkov@bk.ru>
2022-05-28 00:36:39 +0500

    Small code cleanup

---
+++ ParallelBrutePassword/Sources/Extensions/String+Extension.swift
@@ -0,0 +1,23 @@
+//
+//  String+Extension.swift
+//  ParallelBrutePassword
+//
+//  Created by Ilya Volkov on 28.05.2022.
+//
+
+extension String {
+    var digits:      String { return "0123456789" }
+    var lowercase:   String { return "abcdefghijklmnopqrstuvwxyz" }
+    var uppercase:   String { return "ABCDEFGHIJKLMNOPQRSTUVWXYZ" }
+    var punctuation: String { return "!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~" }
+    var letters:     String { return lowercase + uppercase }
+    var printable:   String { return digits + letters + punctuation }
+    
+    
+    
+    mutating func replace(at index: Int, with character: Character) {
+        var stringArray = Array(self)
+        stringArray[index] = character
+        self = String(stringArray)
+    }
+}
---
+++ ParallelBrutePassword/Sources/PasswordGuessing/PasswordGuessing.swift
@@ -0,0 +1,48 @@
+//
+//  PasswordGuessing.swift
+//  ParallelBrutePassword
+//
+//  Created by Ilya Volkov on 28.05.2022.
+//
+
+
+struct PasswordGuessing {
+    
+    func indexOf(character: Character, _ array: [String]) -> Int {
+        return array.firstIndex(of: String(character))!
+    }
+
+    func characterAt(index: Int, _ array: [String]) -> Character {
+        return index < array.count ? Character(array[index]) : Character("")
+    }
+
+    func generateBruteForce(_ string: String, fromArray array: [String]) -> String {
+        var str: String = string
+
+        if str.count <= 0 {
+            str.append(characterAt(index: 0, array))
+        } else {
+            str.replace(at: str.count - 1,
+                        with: characterAt(index: (indexOf(character: str.last!, array) + 1) % array.count, array))
+
+            if indexOf(character: str.last!, array) == 0 {
+                str = String(generateBruteForce(String(str.dropLast()), fromArray: array)) + String(str.last!)
+            }
+        }
+
+        return str
+    }
+    
+    func bruteForce(passwordToUnlock: String) {
+        let ALLOWED_CHARACTERS:   [String] = String().printable.map { String($0) }
+        
+        var password: String = ""
+        
+        while password != passwordToUnlock {
+            password = generateBruteForce(password, fromArray: ALLOWED_CHARACTERS)
+            print(password)
+        }
+        
+        print(password)
+    }
+}
